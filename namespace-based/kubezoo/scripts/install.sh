88f0a1acbc256e3f99fc25968abf69dd49f3dcec
Alex Idowu <alexidowu25@gmail.com>
2024-10-06 09:44:29 +0100

    Update resource file path in install script

--- namespace-based/kubezoo/scripts/install.sh
+++ namespace-based/kubezoo/scripts/install.sh
@@ -8,4 +8,4 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Get the parent directory of the script's directory
 ZOO_ROOT="$(dirname "$SCRIPT_DIR")"
 
-kubectl apply -f $ZOO_ROOT/_output/cluster-resource.yaml
+kubectl apply -f $ZOO_ROOT/_output/quota.yaml
