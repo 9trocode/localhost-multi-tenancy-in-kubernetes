6811828a34b3c0739dd680aa2aad85a239f92330
Alex Idowu <alexidowu25@gmail.com>
2024-10-06 10:38:03 +0100

    Add variables for image tag, architecture, and OS. Pull and tag images

--- namespace-based/kubezoo/scripts/install.sh
+++ namespace-based/kubezoo/scripts/install.sh
@@ -1,3 +1,7 @@
+readonly LOCAL_UP_IMAGE_TAG="v0.2.0"
+readonly LOCAL_ARCH=$(go env GOHOSTARCH)
+readonly LOCAL_OS=$(go env GOHOSTOS)
+
 # Change to the directory of the script
 cd "$(dirname "$0")"
 
@@ -8,3 +12,9 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ZOO_ROOT="$(dirname "$(dirname "$(dirname "$SCRIPT_DIR")")")"
 
 kubectl apply -f $ZOO_ROOT/_output/setup/quota.yaml
+
+# echo "Loading image on $CLUSTER_NAME..."
+docker pull kubezoo/kubezoo:"${LOCAL_UP_IMAGE_TAG}"
+docker pull kubezoo/clusterresourcequota:"${LOCAL_UP_IMAGE_TAG}"
+docker tag kubezoo/kubezoo:"${LOCAL_UP_IMAGE_TAG}" kubezoo/kubezoo:local-up
+docker tag kubezoo/clusterresourcequota:"${LOCAL_UP_IMAGE_TAG}" kubezoo/clusterresourcequota:local-up
