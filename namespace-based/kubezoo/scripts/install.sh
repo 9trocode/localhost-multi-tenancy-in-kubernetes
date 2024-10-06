5b7e68382e745ba38a2bd1cc7c4f9ad332a5e182
Alex Idowu <alexidowu25@gmail.com>
2024-10-06 09:54:13 +0100

    Update root directory path in install script

--- namespace-based/kubezoo/scripts/install.sh
+++ namespace-based/kubezoo/scripts/install.sh
@@ -1,11 +1,10 @@
 # Change to the directory of the script
 cd "$(dirname "$0")"
 
-
 # Get the absolute path of the script's directory
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# Get the parent directory of the script's directory
-ZOO_ROOT="$(dirname "$SCRIPT_DIR")"
+# Get the root directory (Localhost-Multi-tenancy-In-Kubernetes)
+ZOO_ROOT="$(dirname "$(dirname "$(dirname "$SCRIPT_DIR")")")"
 
 kubectl apply -f $ZOO_ROOT/_output/setup/quota.yaml
